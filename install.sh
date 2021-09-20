35265732ec769b872dd75f3102237facce33ee6e
@AnibalTlgram <89871206+AnibalGthub@users.noreply.github.com>
2021-09-20 15:30:41 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "Instalando dependencias"
+echo "Actualizando repositorios"
 sudo apt-get update
+clear
+echo "Instalando dependencias"
 sudo apt-get install php ssh wget unzip gnome-terminal curl dbus-x11 -y
